30cf370243bafc39226664f5c3d5fc55d1e4882e
ArunNejakar <arunnejakar847@gmail.com>
2025-12-26 23:01:39 +0530

    Update traffic_analyzer.py

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -1,3 +1,5 @@
+import os
+
 def analyze_traffic(junction_name, vehicle_count, avg_speed):
     """
     Analyze traffic congestion level based on vehicle count and average speed
@@ -22,3 +24,21 @@ def analyze_traffic(junction_name, vehicle_count, avg_speed):
         "congestion": congestion,
         "suggested_action": action
     }
+
+
+# 🔹 Read Jenkins parameters from environment variables
+junction = os.getenv("JUNCTION_NAME")
+vehicle_count = int(os.getenv("VEHICLE_COUNT"))
+avg_speed = int(os.getenv("AVG_SPEED"))
+
+# 🔹 Call function
+result = analyze_traffic(junction, vehicle_count, avg_speed)
+
+# 🔹 Print output (THIS appears in Jenkins console)
+print("------ Traffic Analysis Report ------")
+print(f"Junction        : {result['junction']}")
+print(f"Vehicle Count   : {result['vehicle_count']}")
+print(f"Average Speed   : {result['average_speed']} km/h")
+print(f"Congestion Level: {result['congestion']}")
+print(f"Suggested Action: {result['suggested_action']}")
+print("-------------------------------------")
